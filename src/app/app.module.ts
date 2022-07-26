5a0d790a471c46a15124ee5063891e01677974c4
kpilszak <klaudia.pilszak@gmail.com>
2022-07-26 20:47:00 +0200

    Add page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { MenuComponent } from './menu/menu.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { RoomsComponent } from './admin/rooms/rooms.component';
 import { UsersComponent } from './admin/users/users.component';
+import { RouterModule, Routes } from "@angular/router";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+const routes: Routes = [
+  { path: 'admin/users', component: UsersComponent },
+  { path: 'admin/rooms', component: RoomsComponent },
+  { path: '', component: CalendarComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +26,11 @@ import { UsersComponent } from './admin/users/users.component';
     UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
